29fb31ccc5a654e2a93d201624173f3d12c88ef5
Ingo <ingohermsen@gmail.com>
2023-07-07 18:43:22 +0200

    edited skills component

--- src/app/components/content/skills/skills.component.ts
+++ src/app/components/content/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Skill } from 'src/app/core/models/skill';
 
 @Component({
   selector: 'app-skills',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
+
+  skills: Skill[] = [
+    {name: "Angular", fileName: 'angular.png'},
+    {name: "Typescript", fileName: 'typescript.png'},
+    {name: "Javascript", fileName: 'javascript.png'},
+    {name: "HTML", fileName: 'html.png'},
+    {name: "Firebase", fileName: 'firebase.png'},
+    {name: "GIT", fileName: 'git.png'},
+    {name: "CSS", fileName: 'css.png'},
+    {name: "Rest-Api", fileName: 'rest-api.png'},
+    {name: "Scrum", fileName: 'scrum.png'},
+    {name: "Material Design", fileName: 'material-design.png'}
+  ]
 }
+
